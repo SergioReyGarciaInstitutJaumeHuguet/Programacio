54686d29ea0aaf2f43a92ce88c8c905406420d99
SergioReyGarciaInstitutJaumeHuguet <144835905+SergioReyGarciaInstitutJaumeHuguet@users.noreply.github.com>
2024-01-30 20:50:20 +0100

    Delete Uf2 directory

--- Uf2/Matriu.py
+++
@@ -1,8 +0,0 @@
-import random
-letra = 0
-a = []
-for i in range(7):
-    letra += random.choice(1,2,3,4,5,6,7)
-for i in range(7):
-    a.append([letra]*7)
-print(a)
--- Uf2/prueba.py
+++
@@ -1,2 +0,0 @@
-matrix = [["-" for i in range(2)]for i in range(4)]
-print(matrix)
