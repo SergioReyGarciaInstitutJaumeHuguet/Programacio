bee4fe73ebe4426a6ec9fa23634704b9272d6059
SergioReyGarciaInstitutJaumeHuguet <srey1@institutjaumehuguet.cat>
2024-02-22 22:36:41 +0100

    no message

--- Uf3/prueba.py
+++ Uf3/prueba.py
@@ -1,24 +1,2 @@
-import json
-import os
-
-# Obtener la ubicación del archivo actual
-directorioActual = os.path.dirname(os.path.abspath(__file__))
-os.chdir(directorioActual)  # Cambiar al directorio actual
-
-numbers = [["Sergio", "77791827P", 18]]
-filename = 'noms.json'
-with open(filename, 'w') as f_obj:
-    json.dump(numbers, f_obj)
-
-try:
-    with open(filename) as archivo:
-        contenido = archivo.read()
-        if contenido:
-            numeros = json.loads(contenido)
-            print(numeros)
-        else:
-            print("El archivo está vacío.")
-except FileNotFoundError:
-    print(f"No se pudo encontrar el archivo {filename}.")
-except json.decoder.JSONDecodeError:
-    print(f"El archivo {filename} no contiene datos JSON válidos.")
+agenda = [{"ID":2,"nombre":"hola"},{"ID":3,"nombre":"adios"}]
+print(agenda[-1]["ID"])
