854aae0acf62482289faa14c49c79a5d250b9fef
SergioReyGarciaInstitutJaumeHuguet <srey1@institutjaumehuguet.cat>
2024-03-07 18:36:02 +0100

    no message

---
+++ UF4/EstructuresDeControl/Exercici16.java
@@ -0,0 +1,12 @@
+package UF4.EstructuresDeControl;
+import java.util.Scanner;
+public class Exercici16 {
+	public static void main(String[] args) {
+		Scanner text = new Scanner(System.in);
+		System.out.println("Inserta los segundos que has estado en llamada");
+		int seconds = text.nextInt();
+		
+		
+	}
+
+}
--- UF4/EstructuresDeControl/Exercici5.java
+++ UF4/EstructuresDeControl/Exercici5.java
@@ -3,20 +3,17 @@ import java.util.Scanner;
 
 public class Exercici5 {
 
-	public static void main(String[] args) {
-		Scanner teclat = new Scanner(System.in);
-		System.out.print("Dime el usuario: ");
-		String user = teclat.nextLine();
-		System.out.print("Ahora la contraseña: ");
-		String password = teclat.nextLine();
-		if(user == "anna" && password == "1234") {
-			System.out.println("Sesión iniciada");
-		}
-		else {
-			System.out.println("No has entrado");
-		}
-		teclat.close();
-
-	}
-
+    public static void main(String[] args) {
+        Scanner teclat = new Scanner(System.in);
+        System.out.print("Dime el usuario: ");
+        String user = teclat.nextLine();
+        System.out.print("Ahora la contraseña: ");
+        String password = teclat.nextLine();
+        if (user.equals("anna") && password.equals("1234")) {
+            System.out.println("Sesión iniciada");
+        } else {
+            System.out.println("No has entrado");
+        }
+        teclat.close();
+    }
 }
