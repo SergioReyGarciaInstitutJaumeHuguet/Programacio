77cd8185395d6af372ade8120a2941ae787bee22
SergioReyGarciaInstitutJaumeHuguet <srey1@institutjaumehuguet.cat>
2024-04-09 20:39:17 +0200

    Notes Alumnes

---
+++ UF5/NotesAlumnes/main.java
@@ -0,0 +1,27 @@
+package UF5.NotesAlumnes;
+import java.util.HashMap;
+import java.util.Map;
+
+public class main {
+	private static HashMap<String, Float> alumnesNota = new HashMap<String, Float>();
+	private static void veureAlumnes() {
+	    for (Map.Entry<String, Float> entry : alumnesNota.entrySet()) {
+	        String nombre = entry.getKey();
+	        Float nota = entry.getValue();
+	        System.out.println("Alumno: " + nombre + ", Nota: " + nota);
+	    }
+	}
+	
+	public static void main(String[] args) {
+		Alumne a1 = new Alumne("Sergio",9);
+		alumnesNota.put(a1.getNom(),a1.getNota());
+		Alumne a2 = new Alumne("Pol",7);
+		alumnesNota.put(a2.getNom(),a2.getNota());
+		Alumne a3 = new Alumne("Ainhoa",6);
+		alumnesNota.put(a3.getNom(),a3.getNota());
+		
+		veureAlumnes();
+
+	}
+
+}
