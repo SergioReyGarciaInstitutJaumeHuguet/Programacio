f043363137c9a02ac543725192438c0c87b962aa
SergioReyGarciaInstitutJaumeHuguet <srey1@institutjaumehuguet.cat>
2024-04-11 19:38:13 +0200

    Nota alumne millorada

--- UF5/NotesAlumnes/main.java
+++ UF5/NotesAlumnes/main.java
@@ -19,6 +19,10 @@ public class main {
 		alumnesNota.put(a2.getNom(),a2.getNota());
 		Alumne a3 = new Alumne("Ainhoa",6);
 		alumnesNota.put(a3.getNom(),a3.getNota());
+		Alumne a4 = new Alumne("Alejandro",5);
+		alumnesNota.put(a4.getNom(),a4.getNota());
+		Alumne a5 = new Alumne("Zaida",3);
+		alumnesNota.put(a5.getNom(), a5.getNota());
 		
 		veureAlumnes();
 
